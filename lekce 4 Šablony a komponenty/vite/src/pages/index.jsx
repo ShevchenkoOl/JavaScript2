2ec44c57f23442d6fdf83d42ba07f5da44197b81
ShevchenkoOL <uzlabini@gmail.com>
2024-05-08 20:49:49 +0200

    modified vite

--- lekce 4 Šablony a komponenty/vite/src/pages/index.jsx
+++ lekce 4 Šablony a komponenty/vite/src/pages/index.jsx
@@ -11,15 +11,12 @@ const Header = () => {
   );
 };
 
+
 const fontStyle = 'intro';
 
 document.querySelector('#root').innerHTML = render(
   <div className="container">
     <Header />
-    {/* <header>
-      <div className="logo"></div>
-      <h1>Alex kouch</h1>
-    </header> */}
     <main>
       <p className={fontStyle}>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
     </main>
